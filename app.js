1813678c7e04e89fa280c8e326cfbb34b5c70739
Ivan Storck <ivanoats@gmail.com>
2014-02-22 18:20:41 -0800

    read added

--- app.js
+++ app.js
@@ -25,6 +25,29 @@ app.get('/api', function (request, response) {
   response.send('my API is running');
 });
 
+// schema and product model
+var Schema = mongoose.Schema;
+
+var Product = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  style: {type: String, unique: true },
+  modified: { type: Date, default: Date.now }
+});
+
+var ProductModel = mongoose.model('Product', Product);
+
+app.get('/api/products', function (request, response){
+  'use strict';
+  return ProductModel.find(function(err, products) {
+    if(!err) {
+      return response.send(products);
+    } else {
+      return console.log(err);
+    }
+  });
+});
+
 // Launch createServer
 
 var port = 3000;
