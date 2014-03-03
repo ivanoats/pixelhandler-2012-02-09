248f2939644ab7de89b093fdd038d73de032e4ca
Ivan Storck <ivanoats@gmail.com>
2014-03-03 13:13:29 -0800

    end of tutorial

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var application_root = __dirname,
             express  = require('express'),
             path     = require('path'),
@@ -12,7 +14,6 @@ mongoose.connect('mongodb://localhost/my_db');
 // config
 
 app.configure(function () {
-  'use strict';
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -21,7 +22,6 @@ app.configure(function () {
 });
 
 app.get('/api', function (request, response) {
-  'use strict';
   response.send('my API is running');
 });
 
@@ -37,8 +37,8 @@ var Product = new Schema({
 
 var ProductModel = mongoose.model('Product', Product);
 
+// READ INDEX
 app.get('/api/products', function (request, response){
-  'use strict';
   return ProductModel.find(function(err, products) {
     if(!err) {
       return response.send(products);
@@ -48,8 +48,8 @@ app.get('/api/products', function (request, response){
   });
 });
 
+// CREATE
 app.post('/api/products', function (request, response){
-  'use strict';
   var product;
   console.log('POST: ');
   console.log(request.body);
@@ -66,6 +66,47 @@ app.post('/api/products', function (request, response){
     }
   });
   return response.send(product);
+});
+
+// READ by ID
+app.get('/api/products/:id', function (request, response) {
+  return ProductModel.findById(request.params.id, function (err, product) {
+    if (!err) {
+      return response.send(product);
+    } else {
+      return console.log(err);
+    }
+  });
+});
+
+// UPDATE by ID
+app.put('/api/products/:id', function (request, response){
+  return ProductModel.findById(request.params.id, function (err, product) {
+    product.title       = request.body.title;
+    product.description = request.body.description;
+    product.style       = request.body.style;
+    return product.save(function (err) {
+      if (!err) {
+        console.log('updated');
+      } else {
+        console.log(err);
+      }
+      return response.send(product);
+    });
+  });
+});
+
+app.delete('/api/products/:id', function (request, response){
+  return ProductModel.findById(request.params.id, function(err, product) {
+    return product.remove(function (err) {
+      if (!err) {
+        console.log('removed');
+        return response.send('');
+      } else {
+        console.log(err);
+      }
+    });
+  });
 });
 
 // Launch createServer
